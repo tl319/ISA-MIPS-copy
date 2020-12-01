a1fa48467174eaa23705512ed2976cdd1d9241d1
David <david.mael.cormier@gmail.com>
2020-11-30 19:04:29 -0500

    updated a/b bus convention

--- ALU.v
+++ ALU.v
@@ -1,7 +1,8 @@
 //MIPS ALU operations: 
 //ADD, SUB, AND, NOR, OR, XOR, MULT, DIV, Shifts
 //a/b are rs/rt, rs/imm, etc. assumed to have been appropriately routed to the ALU 
-//convention: rs is kept as a and rt/imm as b when only one of the three is used, eg SLL
+//convention: a is the value operated on (shifts) or the rs/only source register for other instructions
+//b is the other value, regardless of origin
 //ctrl is a signal from the control path, rather than a field of the instructon
 
 module ALU(
@@ -16,11 +17,11 @@ module ALU(
         4'b0011: out = a | b;   //OR bitwise
         4'b0100: out = a ^ b;   //XOR bitwise
         4'b0101: out = ~(a|b);  //NOT bitwise
-        4'b0110: out = b<<a; //SLL 
-        4'b0111: out = b>>a; //SRL
-        4'b1000: out = b>>>a; //SRA
+        4'b0110: out = a<<b; //SLL 
+        4'b0111: out = a>>b; //SRL
+        4'b1000: out = a>>>b; //SRA
         //not certain how MULT and DIV are implemented, 64 bit out bus containing high and low? 
         //discuss when approching registers
-        4'b1001: out = b<<16; //LUI
+        4'b1001: out = a<<16; //LUI
 
 endmodule
