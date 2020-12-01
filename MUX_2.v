667d2a32cb981b74f42ce34d655cd5750a6f5905
vp219 <vajiravidh.pongpaew19@imperial.ac.uk>
2020-12-01 22:17:49 +0700

    1)corrected ALU syntax, 2)fixed MUX_2.v, 3)implemented MUX_4.v

--- MUX_2.v
+++ MUX_2.v
@@ -1,8 +1,14 @@
-module ALU(
-    input logic [1:0] a, b,
+module MUX_2(
+    input logic [31:0] a, b,
     input logic select,
-    output logic [1:0] out
-):
-
+    output logic [31:0] out
+);
+    
+    always_comb begin
+        case(select)
+        0:  out = a;
+        1:  out = b;
+        endcase
+    end
 
 endmodule
