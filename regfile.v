7f8d278491d060db1b8fdc4a8c37d77591795db3
David <david.mael.cormier@gmail.com>
2020-12-02 16:29:32 -0500

    regfile v1

---
+++ regfile.v
@@ -0,0 +1,30 @@
+module regfile (
+    input logic clk, rst, wren,
+    input logic [4:0] rr1, rr2, wr,
+    input logic [31:0] wd,
+    output logic [31:0] rs, rt, 
+);
+
+    //register vector and integer read/write values
+    logic [31:0] registers [31:0];
+    integer irr1, irr2, iwr;
+
+    //reset registers to 0 during powerup?
+  
+    //read from
+    always_comb (rr1, rr2) begin
+        //is this necessary? can it assign negative values? 
+        irr1 = rr1;
+        irr2 = rr2;
+
+        rs = registers[ irr1 ];
+        rt = registers[ irr2 ];  
+    end
+
+    //write to
+    always_ff @(posedge clk) begin
+        if (wren == 1)
+            //is this necessary? can it assign negative values?
+            irw = rw;
+            registers [irw] <= wd;
+    end
