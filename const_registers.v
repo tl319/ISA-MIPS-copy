e56a0a9121db693b05fde325820b38b5ee1810bf
vp219 <vajiravidh.pongpaew19@imperial.ac.uk>
2020-12-02 19:21:13 +0700

    modified const_registers, implemented high_low_reg(check inputs)

--- const_registers.v
+++ const_registers.v
@@ -1,13 +1,18 @@
-module const_reg_1(
+module const_reg(
     input logic clk,
-    //input logic [31:0] p,
-    output logic [31:0] const_1, const_4, const_31
+    output logic [31:0] out_const_1, out_const_4, out_const_31
 );
 
+    logic [31:0] const_1, const_4, const_31;
+
+    assign const_1 = 8'h00000001;
+    assign const_4 = 8'h00000004;
+    assign const_31 = 8'h0000001F;
+
     always_ff @(posedge clk) begin
-        const_1 <= 8'h00000001; 
-        const_4 <= 8'h00000004;
-        const_31 <= 8'h0000001F;
+        out_const_1 <= const_1; 
+        out_const_4 <= const_4;
+        out_const_31 <= const_31;
     end
     
 endmodule
