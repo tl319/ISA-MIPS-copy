92c32ca20c88f0b02ee1b89f5909035d8fd04716
David <david.mael.cormier@gmail.com>
2020-12-03 17:45:01 -0500

    ALU v3: MULT and DIV

--- ALU.v
+++ ALU.v
@@ -5,11 +5,52 @@
 //b is the other value, regardless of origin
 //ctrl is a signal from the control path, rather than a field of the instructon
 
-module ALU(
-    input [31:0] a, b,
-    input [3:0] ctrl,
-    output [31:0] out
+module div(
+    input logic [31:0] a, b,
+    input logic signdiv,
+    output logic [31:0] q, r
 );
+    integer limit 31;
+    logic [31:0] rega, regb, valreg;
+
+    //take magnitude of negative operands for signed div
+    if (signdiv == 1)
+        if( a[31] == 1 )
+            rega = ~(a-{16'h0001});
+        if( b[31] == 1 )
+            regb = ~(b-{16'h0001});
+    else
+        rega = a;
+        regb = b;        
+
+    //implement division
+    always_comb begin
+        if ()
+    end
+
+    //negate output if div is signed and signs of operands dissagree
+    if ( signdiv & ( a[31] ^ b[31] ) )
+        q =
+        r = 
+    else
+        q =
+        r = 
+
+endmodule
+
+module ALU(
+    input logic [31:0] a, b,
+    input logic [3:0] ctrl,
+    output logic [31:0] out
+);
+    //signals and module for division
+    logic S;
+    logic [31:0] divq, divr;
+    div divcirc(.a(a), .b(b), .signdiv(S), .q(divq), .r(divr));
+
+    //carry for multiplication
+    logic C;
+
     always_comb begin
         case(ctrl)
             4'b0000: out = a + b;   //ADD
@@ -17,19 +58,36 @@ module ALU(
             4'b0010: out = a & b;   //AND bitwise
             4'b0011: out = a | b;   //OR bitwise
             4'b0100: out = a ^ b;   //XOR bitwise
-            4'b0101: out = a<<b; //SLL 
-            4'b0110: out = a>>b; //SRL
-            4'b0111: out = a>>>b; //SRA
-            //not certain how MULT and DIV are implemented, 64 bit out bus containing high and low? 
-            //discuss when approching registers
-            //4'b1000: out = //MULT TOP
-            //4'b1001        //MULT BOT
-            //4'b1010       //MULTU TOP
-            //4'b1011       //MULTU BOT
-            //4'b1100       //DIV
-            //4'b1101       //MOD
-            //4'b1110       //DIVU
-            //4'b1111       //MODU
+            4'b0101: out = a<<b;  //SLL 
+            4'b0110: out = a>>b;  //SRL
+            4'b0111: out = a>>>b; //SRA 
+            4'b1000: out = { { {16{a[31]}}, a[31:16]} * { {16{b[31]}}, b[31:16]} + { {15{1'b0}} , C } }; //MULT TOP
+            4'b1001: begin //MULT BOT
+               out = { {16{a[15]}}, a[15:0]} * { {16{b[15]}}, b[15:0]};
+               C = out[31] //not sure this is legal    
+            end
+            4'b1010: out = { {16'h0000, a[31:16]} * {16'h0000, b[31:16]} + { {15{1'b0}}, C } };        //MULTU TOP
+            4'b1011: begin //MULTU BOT
+                out = {16'h0000, a[15:0]} * {16'h0000, b[15:0]};
+                C = out[31] //not sure this is legal
+            end
+                     
+            4'b1100: begin //DIV
+                S = 1;
+                out = divq;
+            end     
+            4'b1101: begin //MOD
+                S = 1;
+                out = divr;
+            end     
+            4'b1110: begin //DIVU
+                S = 0;
+                out = divq;
+            end      
+            4'b1111: begin //MODU
+                S = 0;
+                out = divr;
+            end      
         endcase
     end
 endmodule
