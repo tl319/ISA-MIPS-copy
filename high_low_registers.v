3820ce20003bb797a0e4eecff398c7187e7b870e
vp219 <vajiravidh.pongpaew19@imperial.ac.uk>
2020-12-04 18:05:16 +0700

    fixed high_low_registers

--- high_low_registers.v
+++ high_low_registers.v
@@ -1,8 +1,16 @@
+//high low register block, with clocked input
+//If WriteEnable is HIGH, write data from 32 bit input bus into the high/low register
+//Data is read from register every clock cycle
+//If Control_High is HIGH, read/write to HIGH register,
+//if Control_High is LOW, read/write to LOW register
+
+//sidenote: check if case inside case loop is legal in verilog
+
 module high_low_registers(
 
     input logic clk,
     input logic ctrl_hi, WrEn,
-    input logic [31:0] input_hi, input_lo,
+    input logic [31:0] input_hi_lo,
     output logic [31:0] out
 
 );
@@ -13,13 +21,13 @@ module high_low_registers(
         case(WrEn)
 
         0:  case(ctrl_hi)
-            0: out <= input_lo;
-            1: out <= input_hi;
+            0: out <= { 16'h0000 , input_hi_lo[15:0] };
+            1: out <= { input_hi_lo[31:16] , 16'h0000 };
             endcase
 
         1:  case(ctrl_hi)
-            0: reg_lo <= input_lo;
-            1: reg_hi <= input_hi;
+            0: reg_lo <= { 16'h0000 , input_hi_lo[15:0] };
+            1: reg_hi <= { input_hi_lo[31:16] , 16'h0000 };
             endcase
         endcase
         
