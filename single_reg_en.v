5441974c8b7472c63ef6cd92750db49b83c5fc8f
vp219 <vajiravidh.pongpaew19@imperial.ac.uk>
2020-12-06 17:48:15 +0700

    because thomas asked for it lol

--- single_reg_en.v
+++ single_reg_en.v
@@ -1,17 +1,18 @@
-module single_reg(
-    input logic clk, rst,
+module single_reg_en(
+    input logic clk, rst, WrEn,
     input logic [31:0] p,
     output logic [31:0] q
 );
 
 	always_ff @(posedge clk, posedge rst) begin	
-
 		if (rst == 1) begin
 			q <= 32'h00000000;
-		end else begin
-			q <= p; 
+		end 
+        else begin
+            if (WrEn) begin
+                q <= p; 
+            end
 		end
-			
 	end
-	
+    
 endmodule
