f71f820320e0e77b48a88ba6c4546f6a26a7e235
David <david.mael.cormier@gmail.com>
2020-12-07 12:26:48 -0500

    DIVU v2: uses OOM, broken but not completely

--- divu.v
+++ divu.v
@@ -4,23 +4,34 @@ module divu(
     output logic [31:0] q, r
 );
     logic [31:0] rega, regb, quotient;
-	 
+
 	 //takes many cycles, requires rst to be high during clk posedge
-	 always_ff@ ( posedge clk ) begin
+	 
+	 always_comb begin
+		q = quotient;
+		r = rega;
+	 end
+	 
+	 always_ff@ ( posedge clk, posedge rst ) begin
 			if(rst == 1) begin
 				quotient <= 32'h00000000;
 				rega <= a;
 				regb <= b;
-			end
-	 
-	 
-			if (rega > regb) begin
-				rega <= (rega - regb);
-				quotient <= (quotient + 1);
-			end	 
+			end else begin
 			
-			q = quotient;
-			r = rega;
+				if (rega >= regb) begin
+					rega <= (rega - regb);
+					quotient <= ((quotient << 1) + 1); 
+
+				end else begin
+					rega <= rega;
+					quotient <= (quotient << 1);
+					
+				end
+				regb <= (regb >> 1);
+			
+			end
+						
 	 end
 
 endmodule
