71a5accaefa0179f6ce24de0c656a4736d9c1b13
pr2518 <pr2518@imperial.ac.uk>
2020-12-07 10:57:59 +0000

    changed output format, added tohex1

--- test/utils/mem_wr_out.cpp
+++ test/utils/mem_wr_out.cpp
@@ -4,10 +4,20 @@
 
 using namespace std;
 
+string to_hex1(unsigned char x)
+{
+      char tmp[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+      string res;
+      res.push_back(tmp[(x>>28)&0xF]);
+      res.push_back(tmp[(x>>24)&0xF]);
+      return res;
+}
+
 void ram_write_out(const vector<unsigned char>& mem)
 {
-  for(int i = 0xBFC00000-1024; i<0xBFC00000+1024; i++)
+  for(unsigned int i = 0xBFC00000-1024; i<0xBFC00000+1024; i++)
   {
-    std::cout<<(uint32_t)mem[i] << std::endl;
+    if(mem[i]!=0)
+      cout<<i<<":"<<to_hex1(mem[i]) << endl;
   }
 }
--- test/utils/mips.hpp
+++ test/utils/mips.hpp
@@ -36,6 +36,8 @@ uint16_t mips_r_instr_to_fncode(const string &s);
 // Returns the hex string of uint16_t 'x'
 string to_hex8(uint32_t x);
 
+string to_hex1(unsigned char x);
+
 // Returns a vector words in sentence 's'
 vector<string> string_break(string s);
 
