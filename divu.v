b0315859c2552c64435938d5d11f2a7de1220317
David <david.mael.cormier@gmail.com>
2020-12-07 15:28:58 -0500

    DIVU v3: stops when div is done, requires original and preshifted amodule divu(

--- divu.v
+++ divu.v
@@ -1,15 +1,18 @@
 module divu(
-    input logic [31:0] a, b, 
+    //division algorithm requires a that has been shifted left such that newa<b<2*newa
+    input logic [31:0] a, b, realb,
 	 input logic clk, rst,
-    output logic [31:0] q, r
+    output logic [31:0] q, r,
+	 output logic done
 );
-    logic [31:0] rega, regb, quotient;
+    logic [31:0] rega, regb, quotient, finished;
 
 	 //takes many cycles, requires rst to be high during clk posedge
 	 
 	 always_comb begin
 		q = quotient;
 		r = rega;
+		done = finished;
 	 end
 	 
 	 always_ff@ ( posedge clk, posedge rst ) begin
@@ -17,19 +20,25 @@ module divu(
 				quotient <= 32'h00000000;
 				rega <= a;
 				regb <= b;
+				finished <= 0;
 			end else begin
 			
-				if (rega >= regb) begin
-					rega <= (rega - regb);
-					quotient <= ((quotient << 1) + 1); 
-
+				if(regb < realb) begin
+					//make this happen a cycle earlier
+					finished <= 1;
 				end else begin
-					rega <= rega;
-					quotient <= (quotient << 1);
-					
+					if (rega >= regb) begin
+						rega <= (rega - regb);
+						quotient <= ((quotient << 1) + 1); 
+
+					end else begin
+						rega <= rega;
+						quotient <= (quotient << 1);
+						
+					end
+					regb <= (regb >> 1);
 				end
-				regb <= (regb >> 1);
-			
+						
 			end
 						
 	 end
