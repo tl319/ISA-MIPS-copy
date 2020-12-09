a737b79fda195238e8c4ba212089c8d2399b996b
hubertsumarno <hubertjosumarno@gmail.com>
2020-12-09 14:18:38 +0000

    changed memory_experiment2 to use newer version of memory_block2

--- memory_experiment2.v
+++ memory_experiment2.v
@@ -1,5 +1,6 @@
 module mips_memory (
     input logic clk,
+    input logic active,
     input logic[31:0] address,
     input logic wr_en,
     input logic read_en,
@@ -12,6 +13,9 @@ module mips_memory (
 
     reg[7:0] memory [2047:0];
 
+    logic[31:0] simp_address;
+
+    assign simp_address=(address<32'hBCF00000) ? address : address-32'hBFC00000 + 32'h00000400;
     initial begin
         integer i;
         /* Initialise to zero by default */
@@ -21,35 +25,55 @@ module mips_memory (
         /* Load contents from file if specified */
         if (RAM_INIT_FILE != "") begin
             $display("RAM : INIT : Loading RAM contents from %s", RAM_INIT_FILE);
-            $readmemh(RAM_INIT_FILE, memory);
+            $readmemh(RAM_INIT_FILE, memory,1024,2047);
         end
     end
 
     always @(posedge clk) begin
         // $display("%h",address);
         if (wr_en) begin
-            if (byte_en[0]) begin
-                memory[address] <= data_in[31:23];
+            if (byte_en[3]) begin
+                memory[simp_address] <= data_in[31:23];
+            end
+            if (byte_en[2]) begin
+                memory[simp_address+1] <= data_in[23:15];
             end
             if (byte_en[1]) begin
-                memory[address+1] <= data_in[23:15];
+                memory[simp_address+2] <= data_in[15:7];
             end
             if (byte_en[0]) begin
-                memory[address+2] <= data_in[15:7];
-            end
-            if (byte_en[0]) begin
-                memory[address+3] <= data_in[7:0];
+                memory[simp_address+3] <= data_in[7:0];
             end
         end 
         if (read_en) begin
-            $display("Inside RAM address: %h",address);
-            data_out <= {memory[address], memory[address+1], memory[address+2], memory[address+3]};
+            $display("inside ram address %h",simp_address);
+            data_out <= {memory[simp_address], memory[simp_address+1], memory[simp_address+2], memory[simp_address+3]};
         end
+    end
+
+    always @(!active)begin
+        // if ( !active ) begin
+            integer i;
+           
+            $display("Displaying Memory contents : ");
+            // $display("address(hex): memory_content(hex) ");
+            for (i = 0 ; i < 1024; i++) begin
+                if(memory[i] != 0 ) begin
+                    $display("%h: %h", i ,memory[i]);
+                end
+            end
+           
+            for (i = 1024 ; i < 2048; i++) begin
+                if(memory[i] != 0 ) begin
+                    $display("%h: %h", (i-1024+3169845248) , memory[i]);
+                end
+            end
     end
 endmodule
 
 module mips_memory_tb;
     logic clk;
+    logic active;
     logic wr_en;
     logic read_en;
     logic[31:0] address;
@@ -81,5 +105,5 @@ module mips_memory_tb;
         $display("data_out: %h", data_out);
     end
 
-    mips_memory memInst(clk, address, wr_en, read_en, byte_en, data_in, data_out);
+    mips_memory memInst(clk, active, address, wr_en, read_en, byte_en, data_in, data_out);
 endmodule
