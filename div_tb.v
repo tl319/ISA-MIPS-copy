5c656947c56fbdbc149972772a6a419a7d27a047
David <david.mael.cormier@gmail.com>
2020-12-10 18:04:01 +0000

    div_tb v1

---
+++ div_tb.v
@@ -0,0 +1,34 @@
+module div_tb;
+
+    logic clk, rst, signdiv, done;
+    logic [31:0] dividend, divisor, quotient, remainder;
+
+    div division(
+        .a(dividend), .b(divisor),
+        .clk(clk), .rst(rst), .signdiv(signdiv),
+        .q(quotient), .r(remainder),
+        .done(done)
+    );
+
+    initial begin
+        clk = 0;
+        repeat (70) begin
+            #10
+            clk = !clk;
+            #10
+            clk = !clk;
+        end
+    end
+
+    initial begin
+        rst <= 0;
+        @(posedge clk);
+        rst <= 1;
+        @(posedge clk);
+        rst <= 0;
+
+        $dumpfile("div_waves.vcd");
+        $dumpvars(0,division);
+    end
+
+endmodule 
