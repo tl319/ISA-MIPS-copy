59ccf860bc64f5e373c12bcb8e2be830c0444fbe
tszwangko <twk219@ic.ac.uk>
2020-12-13 19:24:37 +0800

    little endian ram, big endian assembler

--- test/test_temp.sh
+++ test/test_temp.sh
@@ -2,9 +2,28 @@
 #!/bin/bash
 set -eou pipefail
 
+CPU_SRC="./../rtl"
 TESTCASE="$1" #name of the testcase
 
-FILE="./bin/assembler1"
+BIN_DIRECTORY="./bin"
+if [ -d "${BIN_DIRECTORY}" ]
+then
+>&2 echo "bin directory already created."
+else
+>&2 echo "Creating bin directory..."
+mkdir bin
+fi 
+
+BINARY_DIRECTORY="./binary"
+if [ -d "${BINARY_DIRECTORY}" ]
+then
+>&2 echo "binary directory already created"
+else
+>&2 echo "Creating binary directory..."
+mkdir binary
+fi 
+
+FILE="./bin/assembler"
 if [ -f "$FILE" ]
 then
 >&2 echo "Assembler already created."
@@ -13,8 +32,19 @@ else
 g++ ./utils/assembler_v2.cpp ./utils/mips_assembly.cpp -o ./bin/assembler
 fi 
 
+FILE="./bin/output_filter"
+if [ -f "$FILE" ]
+then
+>&2 echo "Assembler already created."
+else
+>&2 echo "Creating assembler..."
+g++ ./utils/output_filter.cpp ./utils/mips_filter.cpp -o ./bin/output_filter
+fi 
+
+
 >&2 echo "Assembling instructions..."
-./bin/assembler ./testcases/${TESTCASE}.asm.txt > ./binary/${TESTCASE}.hex.txt
+./bin/assembler ./testcases/${TESTCASE}.asm.txt >| ./binary/${TESTCASE}.hex.txt
+
 
 >&2 echo "CPU being tested for ${TESTCASE} testcase..."
 
@@ -23,16 +53,19 @@ fi
 # -s specifies exactly which testbench should be top-level
 # The -P command is used to modify the RAM_INIT_FILE parameter on the test-bench at compile-time
 iverilog -g 2012 \
-   ./../*.v \
+   ${CPU_SRC}/*.v ./test_bench/*.v\
    -s mips_tb \
    -Pmips_tb.RAM_INIT_FILE=\"binary/${TESTCASE}.hex.txt\" \
    -o ./verilog_sim/mips_tb_${TESTCASE} 
 
 
->&2 echo "Running test-bench"
+>&2 echo "Running test-bench..."
 if [[ $? -eq 0 ]]
 then
    ./verilog_sim/mips_tb_${TESTCASE} >| ./output/${TESTCASE}.stdout
 else
    echo "something went wrong, please check name of test case"
 fi
+
+>&2 echo "Extracting memory output..."
+./bin/output_filter ./output/${TESTCASE}.stdout >| ./output/${TESTCASE}.out
--- test/utils/mips.hpp
+++ test/utils/mips.hpp
@@ -58,6 +58,10 @@ bool mips_is_mem_acc_instr(const string &s);
 //initialises data and instruction memory according to the binary file from src
 void mips_mem_init(istream& src, vector<unsigned char>& mem);
 
+//OUTPUT_FILTER
+
+pair<string, string> get_address (string s);
+
 
 
 //void disassemble_instr(const uint32_t& instr, uint& opcode, uint& dest, uint& source1, , uint& source2, uint& address, uint& immediate, )
